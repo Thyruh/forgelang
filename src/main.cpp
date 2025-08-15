6b8e119459657523259d7809ef1b559a12593a04
github_username_here <email@example.com>
2025-08-16 00:52:02 +0200

    Saving the file in a readable string.

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include<cstdio>
+#include<cstdlib>
+#include<iterator>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<sstream>
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    printf("\n");
+    fprintf(stderr, "Error: One argument required: a file to compile.\n");
+    printf("       Correct syntax: ./forge <name>.forge\n");
+    return EXIT_FAILURE;
+  }
+
+  std::fstream strm;
+
+  strm.open(argv[1], std::ios_base::in);
+  printf("\n%s\n", argv[1]);
+  if (!strm.is_open()) {
+    printf("Log");
+    return 1;
+  }
+
+  std::stringstream contents_string;
+
+  contents_string << strm.rdbuf();
+  std::string contents = contents_string.str();
+
+  printf("Read file: %s.\n", argv[1]);
+  printf("Contents:\n\n\"%s \"\n", contents.c_str()); // Lets pretend I never wrote that
+  return EXIT_SUCCESS;
+}
