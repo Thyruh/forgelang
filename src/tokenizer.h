8fa249c38a844d9a12a27bac126e2bd2f018cc54
Thyruh <mebiysy@gmail.com>
2026-04-09 02:43:21 +0200

    Latest future types.

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -26,19 +26,24 @@ typedef enum {
    let,
    equals,
 
-   i8_, char_, // __INT8_TYPE__
-   i16_, // __INT16_TYPE__
-   i32_, // __INT32_TYPE__
-   i64_, // __INT64_TYPE__
-   u8_, // __UINT8_TYPE__
-   u16_, // __UINT16_TYPE__
-   u32_, // __UINT32_TYPE__
-   u64_, // __UINT64_TYPE__
+   // types
+
+   i8_ , char_,   // __INT8_TYPE__
+   i16_,          // __INT16_TYPE__
+   i32_,          // __INT32_TYPE__
+   i64_,          // __INT64_TYPE__
+   u8_ , uchar_,  // __UINT8_TYPE__
+   u16_,          // __UINT16_TYPE__
+   u32_,          // __UINT32_TYPE__
+   u64_,          // __UINT64_TYPE__
    f32_,
    f64_,
    uptr,
 
+   bool_,
+
    string,
+   ustring,
 
    TERMINATE
 } TokenType;
@@ -56,8 +61,8 @@ typedef struct {
 
 typedef struct {
    TokenType type;
-   const char* value;
    TokenPos pos;
+   const char* value;
 } Token;
 
 typedef struct {
